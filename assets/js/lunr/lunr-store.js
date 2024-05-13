fe79de92fe02b71d71905fec30f79276fc452003
odarasimi <odarasimi@users.noreply.github.com>
2024-05-13 14:21:01 +0000

    Update GitHub pages from Jekyll build for commit 8be2f7665e6a0e15cf21ad6eef911b257832cc71

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Random",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "excerpt":"WORK IN PROGRESS :) To add new posts, simply add a file in the _posts directory that follows the convention YYYY-MM-DD-name-of-post.ext and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works. You’ll find this post in your...","categories": ["jekyll","update"],
         "tags": [],
         "url": "/pages/odarasimi/jekyll/update/2024/05/09/welcome-to-jekyll.html",
         "teaser": null
