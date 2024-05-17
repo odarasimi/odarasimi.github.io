9c126df3afc95bd96881a92c26f8d83c39d29a88
odarasimi <odarasimi@users.noreply.github.com>
2024-05-17 10:18:50 +0000

    Update GitHub pages from Jekyll build for commit 69022ad14b7b6013b4ad54905c4a064932d39803

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Random",
-        "excerpt":"WORK IN PROGRESS :) To add new posts, simply add a file in the _posts directory that follows the convention YYYY-MM-DD-name-of-post.ext and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works. You’ll find this post in your...","categories": ["jekyll","update"],
+        "title": "Compilers being Compiled",
+        "excerpt":"x-86 :) To add new posts, simply add a file in the _posts directory that follows the convention YYYY-MM-DD-name-of-post.ext and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works. You’ll find this post in your _posts directory....","categories": ["jekyll","update"],
         "tags": [],
         "url": "/pages/odarasimi/jekyll/update/2024/05/09/welcome-to-jekyll.html",
         "teaser": null
