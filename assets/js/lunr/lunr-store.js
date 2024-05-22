96459fe9d65ac062d073db763d9f7cbb39ed3a75
odarasimi <odarasimi@users.noreply.github.com>
2024-05-22 23:38:05 +0000

    Update GitHub pages from Jekyll build for commit 5db06eac6d40a5c4310f82739133cfcc573c9f4f

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,9 +5,9 @@ var store = [{
         "url": "/pages/odarasimi/jekyll/update/2024/05/20/welcome-to-jekyll.html",
         "teaser": null
       },{
-        "title": "Test post",
-        "excerpt":" ","categories": [],
+        "title": "Compilers not being Compiled",
+        "excerpt":"RISC-V :)   To add new posts, simply add a file in the _posts directory that follows the convention YYYY-MM-DD-name-of-post.ext and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works.  ","categories": ["jekyll","update"],
         "tags": [],
-        "url": "/pages/odarasimi/2024/05/21/test-post.html",
+        "url": "/pages/odarasimi/jekyll/update/2024/05/21/test-post.html",
         "teaser": null
       }]
