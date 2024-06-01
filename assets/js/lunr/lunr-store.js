f09e7b2f5aeb397c82142ca69da7980e9af7c114
odarasimi <odarasimi@users.noreply.github.com>
2024-06-01 16:40:52 +0000

    Update GitHub pages from Jekyll build for commit 5788af8634ba7d91fb4fce8577470df9f5b299ca

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,13 +1,13 @@
 var store = [{
-        "title": "In Progress: Limits",
-        "excerpt":"WIP   Are limits an approximation or an ideal…  ","categories": ["Calculus"],
-        "tags": [],
-        "url": "/pages/odarasimi/calculus/2024/05/21/Limits.html",
-        "teaser": null
-      },{
         "title": "Introductory thoughts on Projective Geometry",
         "excerpt":"… “No man ever steps in the same river twice, for it’s not the same river and he’s not the same man.” - Heraclitus When an object is transformed in some way, we observe a change in the object…but does the object really change, or does our perception of the...","categories": ["geometry"],
         "tags": [],
         "url": "/pages/odarasimi/geometry/2024/05/30/Introductory-thoughts-on-projective-geometry.html",
+        "teaser": null
+      },{
+        "title": "PyTorch is Ten out of ATen",
+        "excerpt":"“I have made this letter longer than usual, only because I have not had the time to make it shorter.” - Blaise Pascal For anyone with passing familiarity with deep learning workloads and the challenge of scaling, it is generally no surprise to see AI companies gathered at the cathedral...","categories": ["Programming"],
+        "tags": [],
+        "url": "/pages/odarasimi/programming/2024/06/01/PyTorch-is-Ten-out-of-ATen.html",
         "teaser": null
       }]
