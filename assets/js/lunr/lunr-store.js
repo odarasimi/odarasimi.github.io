da8919b6c02e008135e9350faacb08f559dc6c5c
odarasimi <odarasimi@users.noreply.github.com>
2024-07-17 16:46:31 +0000

    Update GitHub pages from Jekyll build for commit 41f96364fe95f5acdbde2c49c9b16b4fdb940eeb

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Mild thoughts on Projective Geometry",
-        "excerpt":"… “No man ever steps in the same river twice, for it’s not the same river and he’s not the same man.” - Heraclitus “When an object is transformed, we observe a change in it. However, does any property of the object truly change, or is it our perception of...","categories": ["geometry"],
+        "excerpt":"“No man ever steps in the same river twice, for it’s not the same river and he’s not the same man.” - Heraclitus “When an object is transformed, we observe a change in it. However, does any property of the object truly change, or is it our perception of the...","categories": ["geometry"],
         "tags": [],
         "url": "/pages/odarasimi/geometry/2024/05/30/Mild-thoughts-on-projective-geometry.html",
         "teaser": null
@@ -9,5 +9,17 @@ var store = [{
         "excerpt":"“I have made this letter longer than usual, only because I have not had the time to make it shorter.” - Blaise Pascal For anyone with passing familiarity with deep learning workloads and the challenge of scaling, it is generally no surprise to see AI companies gathered at the cathedral...","categories": ["Programming"],
         "tags": [],
         "url": "/pages/odarasimi/programming/2024/06/01/PyTorch-is-Ten-out-of-ATen.html",
+        "teaser": null
+      },{
+        "title": "Projective Geometry and Transformations of 2D",
+        "excerpt":"“Algebra is the offer made by the devil to the mathematician. The devil says: “I will give you this powerful machine, and it will answer any question you like. All you need to do is give me your soul: give up geometry and you will have this marvellous machine.” ....","categories": ["geometry"],
+        "tags": [],
+        "url": "/pages/odarasimi/geometry/2024/07/17/projective-geometry-and-transformations-of-2D.html",
+        "teaser": null
+      },{
+        "title": "Linear Least Squares",
+        "excerpt":"“An approximate answer to the right problem is worth a good deal more than an exact answer to an approximate problem.” - John Tukey Preamble Lagrange multipliers We have a function $a$ -&gt; $f(x,y)$ and an equation $b$ -&gt; $x^2 + y^2 = 1$. We want values of $x$ and...","categories": ["math"],
+        "tags": [],
+        "url": "/pages/odarasimi/math/2024/07/17/linear-least-squares.html",
         "teaser": null
       }]
